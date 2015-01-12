7d5967c3dce45b7e3aa12add83f65e913c79f783
Tomasz Paweł Gajc <tpgxyz@gmail.com>
2015-01-12 16:39:25 +0400

    sync

--- modules-git.sh
+++ modules-git.sh
@@ -1,7 +1,9 @@
 git clone http://git.enlightenment.org/enlightenment/modules/alarm.git/
 git clone http://git.enlightenment.org/enlightenment/modules/comp-scale.git/
 git clone http://git.enlightenment.org/enlightenment/modules/cpu.git/
+git clone http://git.enlightenment.org/enlightenment/modules/desksanity.git/
 git clone http://git.enlightenment.org/enlightenment/modules/diskio.git/
+git clone http://git.enlightenment.org/enlightenment/modules/edgar.git/
 git clone http://git.enlightenment.org/enlightenment/modules/eenvader.fractal.git/
 git clone http://git.enlightenment.org/enlightenment/modules/elev8.git/
 git clone http://git.enlightenment.org/enlightenment/modules/elfe.git/
@@ -12,6 +14,7 @@ git clone http://git.enlightenment.org/enlightenment/modules/everything-websearc
 git clone http://git.enlightenment.org/enlightenment/modules/eweather.git/
 git clone http://git.enlightenment.org/enlightenment/modules/forecasts.git/
 git clone http://git.enlightenment.org/enlightenment/modules/mail.git/
+git clone http://git.enlightenment.org/enlightenment/modules/mem.git/
 git clone http://git.enlightenment.org/enlightenment/modules/moon.git/
 git clone http://git.enlightenment.org/enlightenment/modules/mpdule.git/
 git clone http://git.enlightenment.org/enlightenment/modules/net.git/
@@ -20,6 +23,9 @@ git clone http://git.enlightenment.org/enlightenment/modules/packagekit.git/
 git clone http://git.enlightenment.org/enlightenment/modules/penguins.git/
 git clone http://git.enlightenment.org/enlightenment/modules/photo.git/
 git clone http://git.enlightenment.org/enlightenment/modules/places.git/
+git clone http://git.enlightenment.org/enlightenment/modules/share.git/
 git clone http://git.enlightenment.org/enlightenment/modules/tclock.git/
+git clone http://git.enlightenment.org/enlightenment/modules/wallpaper2.git/
+git clone http://git.enlightenment.org/enlightenment/modules/wlan.git/
 rm -rf ./*/.git
 rm -f ./*/.gitignore
